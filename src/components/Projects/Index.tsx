bf3a73eff147c2e49d1e51258b8f2c642ad92ef8
kasilianaoliveira <kasilianaoliveira@gmail.com>
2023-02-08 16:06:19 -0300

    Add projects page

---
+++ src/components/Projects/Index.tsx
@@ -0,0 +1,75 @@
+import React from 'react'
+import { Button } from '../Button/Index'
+import { Title } from '../Title/Index'
+import projetoImg from "../../assets/projeto.png"
+import GridImg from "../../assets/icon-grid.svg"
+import ArrowImg from "../../assets/icon-arrow.svg"
+import iconImg from "../../assets/icon.svg";
+
+
+import "./styles.css"
+
+export const Projects = () => {
+  return (
+    <article className='container-projects'>
+      <span className='title-detail-projects'>PROJETOS</span>
+      <Title title='Projetos em destaque' />
+      <div className="cards-projects">
+        <div className="card">
+          <img src={projetoImg} alt="Imagem de um projeto desenvolvido" className='img-card' />
+          <div className='card-infos'>
+            <h3>Portfolio website UI</h3>
+            <span className='info'>
+              <img src={GridImg} alt="" />
+              UI Design
+            </span>
+          </div>
+          <div className='card-open-project'>
+            <span>
+              Veja mais
+              <img src={ArrowImg} alt="" />
+            </span>
+            <Button title="Abrir projeto" hrefOption='teste' />
+          </div>
+        </div>
+        <div className="card">
+          <img src={projetoImg} alt="Imagem de um projeto desenvolvido" className='img-card' />
+          <div className='card-infos'>
+            <h3>Portfolio website UI</h3>
+            <span className='info'>
+              <img src={GridImg} alt="" />
+              UI Design
+            </span>
+          </div>
+          <div className='card-open-project'>
+            <span>
+              Veja mais
+              <img src={ArrowImg} alt="" />
+            </span>
+            <Button title="Abrir projeto" hrefOption='teste' />
+          </div>
+        </div>
+        <div className="card">
+          <img src={projetoImg} alt="Imagem de um projeto desenvolvido" className='img-card' />
+          <div className='card-infos'>
+            <h3>Portfolio website UI</h3>
+            <span className='info'>
+              <img src={GridImg} alt="" />
+              UI Design
+            </span>
+          </div>
+          <div className='card-open-project'>
+            <span>
+              Veja mais
+              <img src={ArrowImg} alt="" />
+            </span>
+            <Button title="Abrir projeto" hrefOption='teste' />
+          </div>
+        </div>
+      </div>
+      <Button title='Repositórios no github' hrefOption='https://github.com/kasilianaoliveira' />
+      <img src={iconImg} alt="detalhe" className='detail-project' />
+
+    </article>
+  )
+}
