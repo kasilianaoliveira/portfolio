3b005b6dcc885924457970fb9d8d442533eb4e48
kasilianaoliveira <kasilianaoliveira@gmail.com>
2023-02-08 16:06:01 -0300

    Add home page

---
+++ src/components/Home/Index.tsx
@@ -0,0 +1,27 @@
+import { Menu } from '../Menu/Index';
+import girlImg from "../../assets/girl.svg";
+
+import "./styles.css";
+
+//adicionar portfolio verdadeiro
+import portfolioPDF from "../../assets/portfolio.pdf"
+import { Title } from '../Title/Index';
+import { Button } from '../Button/Index';
+export const Home = () => {
+  return (
+
+    <article className='container' id='home'>
+
+      <img src={girlImg} alt="Garota em um computador" />
+      <div className='content'>
+        <Title title='Desenvolvedora Front End & UI Designer' isPink />
+        <Button hrefOption={portfolioPDF} title="Download CV" />
+        {/* <button className='button-download' >
+          <a href={portfolioPDF} target="_blank">Download CV</a>
+        </button> */}
+      </div>
+
+    </article>
+
+  )
+}
