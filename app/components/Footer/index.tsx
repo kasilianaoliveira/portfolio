d1d8c757f03b24d3e6b2acd6c72c68f2d234e047
kasilianaoliveira <kasilianaoliveira@gmail.com>
2026-02-18 13:05:22 -0300

    feat: add Footer component with copyright notice and styling

---
+++ app/components/Footer/index.tsx
@@ -0,0 +1,9 @@
+export function Footer() {
+  return (
+    <footer className="flex items-center justify-center p-5 text-purple-100/50 border-t border-purple-300/20">
+      <span>
+        &copy; 2026 Kasiliana Oliveira. Todos os direitos reservados.
+      </span>
+    </footer>
+  )
+}
